64000e1ed19f2ec79f83e69bf1566021a658c51f
Alexander Leyke <alexander_leyke@ultimatesoftware.com>
2020-11-23 18:43:29 -0500

    PRO-28528 - Alex - large object reads were incorrectly subject to short timeouts

--- src/main/java/org/wikimedia/elasticsearch/swift/repositories/blobstore/SwiftBlobContainer.java
+++ src/main/java/org/wikimedia/elasticsearch/swift/repositories/blobstore/SwiftBlobContainer.java
@@ -313,6 +313,12 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
         return keyPath + blobName;
     }
 
+    private static class ObjectInfo {
+        long size;
+        InputStream stream;
+        String tag;
+    }
+
     /**
      * Fetch a given blob into memory, verify etag, and return InputStream.
      * @param blobName The blob name to read
@@ -323,25 +329,27 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
         String objectName = buildKey(blobName);
 
         try {
-            return withTimeout().retry(retryIntervalS, shortOperationTimeoutS, TimeUnit.SECONDS, () -> {
-                try {
-                    return SwiftPerms.execThrows(() -> {
+            ObjectInfo object = withTimeout().retry(retryIntervalS, shortOperationTimeoutS, TimeUnit.SECONDS, () ->
+                SwiftPerms.execThrows(() -> {
+                    try {
                         StoredObject storedObject = blobStore.getContainer().getObject(objectName);
-                        InputStream rawInputStream = storedObject.downloadObjectAsInputStream();
-                        String objectEtag = storedObject.getEtag();
+                        ObjectInfo objectInfo = new ObjectInfo();
+                        objectInfo.stream = storedObject.downloadObjectAsInputStream();
+                        objectInfo.size = storedObject.getContentLength();
+                        objectInfo.tag = storedObject.getEtag();
+                        return objectInfo;
+                    } catch (NotFoundException e) {
+                        // this conversion is necessary for tests to pass
+                        String message = "cannot read object [" + objectName + "]";
+                        logger.warn(message);
+                        NoSuchFileException e2 = new NoSuchFileException(message);
+                        e2.initCause(e);
+                        throw e2;
+                    }
+                }));
 
-                        return streamRead ? new InputStreamWrapperWithDataHash(objectName, rawInputStream, objectEtag)
-                                : objectToReentrantStream(objectName, rawInputStream, storedObject.getContentLength(), objectEtag);
-                    });
-                }
-                catch (NotFoundException e) {
-                    String message = "cannot read object [" + objectName + "]";
-                    logger.warn(message);
-                    NoSuchFileException e2 = new NoSuchFileException(message);
-                    e2.initCause(e);
-                    throw e2;
-                }
-            });
+            return streamRead ? new InputStreamWrapperWithDataHash(objectName, object.stream, object.tag)
+                              : objectToReentrantStream(objectName, object.stream, object.size, object.tag);
         }
         catch (IOException | RuntimeException e){
             throw e;
@@ -351,6 +359,14 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
         }
     }
 
+    /**
+     * Write blob to Swift
+     * @param blobName blob name
+     * @param in in-memory stream with blob data. Do not rely on it staying open after this call!
+     * @param blobSize blob size
+     * @param failIfAlreadyExists throw exception if blob exists
+     * @throws IOException can be thrown from various operations
+     */
     @Override
     public void writeBlob(final String blobName,
                           final InputStream in,
@@ -358,7 +374,7 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
                           boolean failIfAlreadyExists) throws IOException {
         if (executor != null && allowConcurrentIO && !streamWrite) {
             // async execution races against the InputStream closed in the caller. Read all data locally.
-            InputStream capturedStream = streamToReentrantStream(blobName, in, blobSize, true);
+            InputStream capturedStream = streamToReentrantStream(blobName, in, blobSize, true, false);
 
             Future<Void> task = executor.submit(() -> {
                 internalWriteBlob(blobName, capturedStream, blobSize, failIfAlreadyExists);
@@ -374,7 +390,7 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
 
 
     /**
-     * Read object entirely into memory and check its etag.
+     * Read object entirely into memory and check its etag. Elementary read operations are timed.
      * @param objectName full path to the object
      * @param rawInputStream server input stream
      * @param size size hint, do not trust it
@@ -386,7 +402,7 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
                                                 InputStream rawInputStream,
                                                 long size,
                                                 String objectEtag) throws IOException {
-        InputStream objectStream = streamToReentrantStream(objectName, rawInputStream, size, false);
+        InputStream objectStream = streamToReentrantStream(objectName, rawInputStream, size, false, true);
         String dataEtag = DigestUtils.md5Hex(objectStream);
 
         if (dataEtag.equalsIgnoreCase(objectEtag)) {
@@ -407,13 +423,15 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
      * @param in server input stream
      * @param sizeHint size hint, do not trust it
      * @param forceRead force reading into memory
+     * @param useReadTimeout should read be timed? (only if actual I/O is happening, not memory duplication)
      * @return re-entrant stream holding the blob
      * @throws IOException on I/O error
      */
     private InputStream streamToReentrantStream(String objectName,
                                                 InputStream in,
                                                 long sizeHint,
-                                                boolean forceRead) throws IOException {
+                                                boolean forceRead,
+                                                boolean useReadTimeout) throws IOException {
         if (!forceRead && in.markSupported()) {
             logger.debug("Reusing reentrant stream of class [" + in.getClass().getName() + "]");
             return in;
@@ -422,7 +440,6 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
 
         final int bufferSize = (int) blobStore.getBufferSizeInBytes();
         final byte[] buffer = new byte[bufferSize];
-        int read;
         ByteArrayOutputStream baos = new ByteArrayOutputStream(sizeHint > 0 ? (int) sizeHint : bufferSize) {
             @Override
             public synchronized byte[] toByteArray() {
@@ -430,7 +447,8 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
             }
         };
 
-        while ((read = in.read(buffer)) != -1) {
+        int read;
+        while ((read = useReadTimeout ? readWithTimeout(in, buffer) : in.read(buffer)) != -1) {
             baos.write(buffer, 0, read);
         }
 
@@ -441,6 +459,19 @@ public class SwiftBlobContainer extends AbstractBlobContainer {
                     "] bytes instead of [" + sizeHint + "]");
         }
         return new ByteArrayInputStream(bytesRead);
+    }
+
+    private int readWithTimeout(InputStream in, final byte[] buffer) throws IOException {
+        try {
+            return withTimeout().timeout(shortOperationTimeoutS, TimeUnit.SECONDS, () ->
+                    SwiftPerms.execThrows(() -> in.read(buffer)));
+        }
+        catch (IOException e){
+            throw e;
+        }
+        catch(Exception e){
+            throw new BlobStoreException("stream read failed", e);
+        }
     }
 
     private void internalWriteBlob(String blobName, InputStream fromStream, long blobSize, boolean failIfAlreadyExists) throws IOException {
